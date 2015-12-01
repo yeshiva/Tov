b21c51feb26e750bbc29444a954df273a3b00fc2
Romaric Okemba <ngromaricokemba@gmail.com>
2015-12-01 22:39:28 +0000

    Ajout des classes de bases

---
+++ tov/tov-core/src/main/java/org/tov/execptions/TovException.java
@@ -0,0 +1,27 @@
+package org.tov.execptions;
+
+import javax.ejb.ApplicationException;
+
+/**
+ * 
+ * @author <a mailto="ngromaricokemba@gmail.com">Romaric Okemba Architecte</a>
+ * La classe qui gère les exceptions de l'application.
+ * 
+ */
+
+@ApplicationException(rollback=true)
+public class TovException extends Exception {
+	
+	public TovException(){
+		
+	}
+	
+    public TovException(String msg){
+		super(msg);
+	}
+    
+    public TovException(String msg,Throwable cause){
+		super(msg,cause);
+	}
+
+}
